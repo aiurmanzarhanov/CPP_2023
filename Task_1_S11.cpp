45be6e1fd5999e46b17e24db5bc88d1678dae89f
aiurmanzarhanov <125951037+aiurmanzarhanov@users.noreply.github.com>
2023-03-07 07:27:51 +0300

    Create Task_1_S11.cpp

---
+++ Task_1_S11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a=0;
+    for (a;a<=100;a++){
+        if(a%3==0){
+            cout<<a<<" ";
+            }
+    }
+    return 0;
+}
