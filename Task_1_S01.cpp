3115810d25ae65c49f4ca0e548ea2c3d8d316bb4
aiurmanzarhanov <125951037+aiurmanzarhanov@users.noreply.github.com>
2023-03-07 07:25:31 +0300

    Create Task_1_S01.cpp

---
+++ Task_1_S01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a=10;
+    char b=4;
+    bool c=true;
+    float d=4.592;
+    a++;
+    cout<<a<<'\n';
+    b--;
+    cout<<b<<'\n';
+    cout<<c*0<<'\n';
+    cout<<d/2;
+
+    return 0;
+}
