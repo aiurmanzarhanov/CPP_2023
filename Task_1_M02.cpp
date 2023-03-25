fef2587a6496dd5d132759e50d961ffd351fb5e4
aiurmanzarhanov <125951037+aiurmanzarhanov@users.noreply.github.com>
2023-03-25 13:37:56 +0300

    Create Task_1_M02.cpp

---
+++ Task_1_M02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    setlocale(0,"Russian");
+    int N;
+    float A,S;
+    const float pi=3.1416;
+    cout<<"Введитее длину стороны:";
+    cin>>A;
+    cout<<"Введите количество сторон:";
+    cin>>N;
+    if(N==3){
+        S=(A*A*sqrt(3))/4;
+        cout<<"Площадь треугольника:"<<S;
+    }
+    else if (N>3) {
+        S=(A*A*N)/(4*tan(pi/N));
+        cout<<"Площадь многоугольника:"<<S;
+    }
+    else {
+        cout<<"Площадь посчитать невозможно:";
+    }
+    return 0;
+}
