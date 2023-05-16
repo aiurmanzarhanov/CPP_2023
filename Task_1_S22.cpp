a1609262861cbf33a4090238fcfb9563c3aca4c1
aiurmanzarhanov <125951037+aiurmanzarhanov@users.noreply.github.com>
2023-05-16 07:09:23 +0300

    Update Task_1_S22.cpp

--- Task_1_S22.cpp
+++ Task_1_S22.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main() {
     setlocale(0,"Russian");
-    ifstream input_file("output.txt");
+    ifstream input_file("output_S22.txt");
     int a;
     int b = 1;
     while (input_file >> a) {
         cout <<b<<". "<<a<<endl;
         b++;
     }
+    input_file.close();
     return 0;
 }
