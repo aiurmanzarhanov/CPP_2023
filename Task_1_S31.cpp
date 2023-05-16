9250ab3fe000f84bb3c1b3c7d6e8d00a37621a9f
aiurmanzarhanov <125951037+aiurmanzarhanov@users.noreply.github.com>
2023-05-16 07:20:42 +0300

    Update Task_1_S31.cpp

--- Task_1_S31.cpp
+++ Task_1_S31.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 
 using namespace std;
 
-int main()
-{
-setlocale(0, "Russian");
-int a = 10, number;
-ofstream input("output.txt");
-string N[a] {"Ноль", "Один", "Два", "Три", "Четыре", "Пять", "Шесть", "Семь", "Восемь", "Девять"};
-for (int i = 0; i < a; i++)
-{
-    input << N[i] << endl;
-}
-cout << "Введите число от 0 до 9: ";
-cin >> number;
-while (number >= a)
-{
-    cout << "Введите число от 0 до 9: ";
-    cin >> number;
-}
-while (number < 0)
-{
-    cout << "Введите число от 0 до 9: ";
-    cin >> number;
-}
-    cout << "Вы ввели число: " << N[number] << endl;
-return 0;
+int main() {
+    setlocale(0,"Russian");
+    string N[10]={"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+    int num;
+    cout<<"Введите число:";
+    cin>>num;
+    if (num >= 0 && num <= 9) {
+        cout<<"Название числа:" << N[num]<<endl;
+    } else {
+        cout<<"Ошибка, нужно ввести число от 0 до 9"<<endl;
+    }
+    return 0;
 }
